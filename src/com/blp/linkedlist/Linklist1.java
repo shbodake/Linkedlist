86200c7905c7d199d7da7b373ed60e5e960328a1
shbodake <sayalibodake28@gmail.com>
2021-11-05 17:08:32 +0530

    Added: added data at starting of element

---
+++ src/com/blp/linkedlist/Linklist1.java
@@ -0,0 +1,30 @@
+package com.blp.linkedlist;
+
+public class Linklist1 {
+    Node head;
+    Node tail;
+
+    public void push(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            Node temp = head;
+            this.head = newNode;
+            newNode.next = temp;
+        }
+    }
+
+    public void print() {
+        if (head == null) {
+            System.out.println("Linked List is Empty");
+        } else {
+            Node temp = head;
+            while (temp != null) {
+                System.out.print(temp.data+ " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
